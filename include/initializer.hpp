bfd710f372dc4b3d0efd571706b6149baaad2448
mohajel <msadegi007@yahoo.com>
2023-08-07 11:40:51 +0330

    initlizer added

---
+++ include/initializer.hpp
@@ -0,0 +1,19 @@
+#if !defined(__INITIALIZER__)
+#define __INITIALIZER__
+
+#include <string>
+
+class Initializer
+{
+
+public:
+    Initializer();
+    ~Initializer();
+
+
+private:
+};
+
+
+
+#endif // __INITIALIZER__
---
+++ src/initializer.cpp
@@ -0,0 +1,19 @@
+// In the Name of God
+
+#include "initializer.hpp"
+#include "manual.hpp"
+#include "participant.hpp"
+#include "coin.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+Initializer::Initializer()
+{
+}
+
+Initializer::~Initializer()
+{
+}
+
