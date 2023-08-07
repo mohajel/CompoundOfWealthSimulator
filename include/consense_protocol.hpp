667b918a496ebfd80e64ed497ed4969d529f5819
mohajel <msadegi007@yahoo.com>
2023-08-07 14:34:52 +0330

    consense protocol class added

---
+++ include/consense_protocol.hpp
@@ -0,0 +1,19 @@
+#if !defined(__CONSENSE_PROTOCOL__)
+#define __CONSENSE_PROTOCOL__
+
+#include <string>
+#include <vector>
+
+
+#include "participant.hpp"
+#include "coin.hpp"
+class ConsenseProtocol
+{
+
+public:
+    ConsenseProtocol();
+    ~ConsenseProtocol();
+private:
+};
+
+#endif // __CONSENSE_PROTOCOL__
---
+++ src/consense_protocol.cpp
@@ -0,0 +1,24 @@
+// In the Name of God
+
+#include <iostream>
+#include <vector>
+#include <random>
+
+
+#include "consense_protocol.hpp"
+#include "manual.hpp"
+#include "participant.hpp"
+#include "coin.hpp"
+
+
+using namespace std;
+
+ConsenseProtocol::ConsenseProtocol()
+{
+    
+}
+
+ConsenseProtocol::~ConsenseProtocol()
+{
+}
+
