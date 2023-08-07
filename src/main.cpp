4d0233b93cbe2425ca5a63f3dc7b7884e72e1778
mohajel <msadegi007@yahoo.com>
2023-08-07 10:31:51 +0330

    initial base created

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+#include <../include/manual.hpp>
+
+using namespace std;
+
+int main()
+{
+    cout << GAME_HEADER_NAME << endl;
+    
+    return 0;
+}
